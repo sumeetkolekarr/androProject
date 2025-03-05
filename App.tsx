01e60519daaf0c72aefad6c59ff904ab5317f7b9
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-03-05 21:07:55 +0530

    Learned Building Components

--- App.tsx
+++ App.tsx
@@ -6,14 +6,24 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
+import UserData from './components/UserData';
+
+const name = 'Leo';
 
 const App = () => {
+  const fruit = () => {
+    console.warn('Function Called');
+  };
   return (
     <View>
-      <Text>Hello, </Text>
-      <Text>React Native</Text>
+      <Text style={{fontSize: 25, paddingLeft: 20}}>Hello, </Text>
+      <Text style={{fontSize: 20, paddingLeft: 30}}>{name}</Text>
+      <Button title="Press Here" onPress={fruit} color={'purple'}></Button>
+      <Button title="Press Me Too"></Button>
+      <UserData />
     </View>
   );
 };
+
 export default App;
---
+++ components/UserData.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const UserData = () => {
+  return (
+    <View>
+      <Text>Components</Text>
+    </View>
+  );
+};
+
+export default UserData;
